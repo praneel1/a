008347430caa0a8db6c4160b6d90b6ac63c4670a
Praneel Maitra <76672209+praneel1@users.noreply.github.com>
2021-04-12 16:49:29 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let a;
+let para = document.getElementById("p1")
+function showHide() {
+	para.style.display="inline";
+	setTimeOut(hover(),50);
+}
+function hover() {
+
+	para.style.display="none";
+
+}
